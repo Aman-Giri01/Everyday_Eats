57910e432929f90be872202cce0124603100a508
Aman-Giri01 <amankumargiri2022@gmail.com>
2025-03-18 16:44:12 +0530

    style and mode

--- src/main.js
+++ src/main.js
@@ -1,21 +1,24 @@
-// import './style.css'
+// ------ mode change------------
+const mode=document.querySelector('#mode');
+const icon=mode.querySelector('i');
+mode.addEventListener('click',()=>{
+  document.body.classList.toggle('dark-mode');
+  if(document.body.classList.contains('dark-mode')){
+    icon.classList.replace('fa-lightbulb','fa-moon');
+  }
+  else{
+    icon.classList.replace('fa-moon','fa-lightbulb');
+  }
 
-fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata`)
-.then((res)=>{
-   return res.json()})
-.then((data)=>{
-  console.log(data);
-})
-.catch(error=>console.error("Unable to Fetch data..",error));
+});
+// ----------- end mode change -------------------
+
+// fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata`)
+// .then((res)=>{
+//    return res.json()})
+// .then((data)=>{
+//   console.log(data);
+// })
+// .catch(error=>console.error("Unable to Fetch data..",error));
 
 
-// const fetchUser=()=>{
-//   const user_name=`Aman-Giri01`
-
-//   fetch(`https://api.github.com/users/${user_name}`)
-//   .then((res)=>{
-//       return res.json()})
-//   .then((data)=>{
-//      console.log(data);
-//   }).catch(error => console.error("Error fetching data:", error));}
-//   fetchUser();
